b8a017428bf81c4b240ed5a1d3c5ddc4385c35b0
gulnuravci <34399080+gulnuravci@users.noreply.github.com>
2020-05-09 15:02:45 -0400

    almost done

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 21 14:57:17 2020
-
-@author: etill
-"""
-
 #import statements
 from flask import Flask, render_template
 
@@ -16,13 +9,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/page1.html")
+@app.route("/assignments.html")
 def page1():
-    return render_template('page1.html')
+    return render_template('assignments.html')
 
-@app.route("/page2.html")
+@app.route("/classes.html")
 def page2():
-    return render_template('page2.html')
+    return render_template('classes.html')
 
 #start the server
 if __name__ == "__main__":
